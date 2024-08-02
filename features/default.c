5a0a71062363773a89ee78d68c46add051e3186a
xj63 <xj63work@163.com>
2024-08-03 04:10:02 +0800

    refactor: rename pid_update_with_default

--- features/default.c
+++ features/default.c
@@ -1,6 +1,6 @@
 #include "../pid.h"
 
-float pid_update_default(struct Pid *pid, float error, float dt) {
+float pid_update_with_default(struct Pid *pid, float error, float dt) {
   float differential = (error - pid->previous) / dt;
 
   pid->previous = error;
@@ -13,10 +13,10 @@ float pid_update_default(struct Pid *pid, float error, float dt) {
   return proportional + integral + derivative;
 }
 
-float pid_update_default_is_first(struct Pid *pid, float error, float dt) {
+float pid_update_with_default_is_first(struct Pid *pid, float error, float dt) {
   pid->previous = error;
-  pid->update = pid_update_default;
-  return pid_update_default(pid, error, dt);
+  pid->update = pid_update_with_default;
+  return pid_update_with_default(pid, error, dt);
 }
 
 struct Pid pid_new_with_default(float kp, float ki, float kd) {
@@ -28,7 +28,7 @@ struct Pid pid_new_with_default(float kp, float ki, float kd) {
   pid.previous = 0;
   pid.integral = 0;
 
-  pid.update = pid_update_default_is_first;
+  pid.update = pid_update_with_default_is_first;
 
   return pid;
 }
