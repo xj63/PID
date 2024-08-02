742973d1be4f079916c2797cd7777de7c539f519
xj63 <xj63work@163.com>
2024-08-03 02:48:29 +0800

    feat: pid with integral separation when error exceed the limit

---
+++ features/separation.c
@@ -0,0 +1,33 @@
+#include "../pid.h"
+
+float pid_update_with_integral_separation(struct Pid *pid, float error,
+                                          float dt) {
+  float differential = (error - pid->previous) / dt;
+
+  pid->previous = error;
+  if (error < pid->option.integral_separation_error_threshold)
+    pid->integral += error * dt;
+
+  float proportional = pid->kp * error;
+  float integral = pid->ki * pid->integral;
+  float derivative = pid->kd * differential;
+
+  return proportional + integral + derivative;
+}
+
+float pid_update_with_integral_separation_is_first(struct Pid *pid, float error,
+                                                   float dt) {
+  pid->previous = error;
+  pid->update = pid_update_with_integral_separation;
+  return pid_update_with_integral_separation(pid, error, dt);
+}
+
+struct Pid
+pid_new_with_integral_separation(float kp, float ki, float kd,
+                                 float integral_separation_error_threshold) {
+  struct Pid pid = pid_new(kp, ki, kd);
+  pid.option.integral_separation_error_threshold =
+      integral_separation_error_threshold;
+  pid.update = pid_update_with_integral_separation_is_first;
+  return pid;
+}
