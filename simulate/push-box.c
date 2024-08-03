5173d9407fce2c8b3c32593837c5c9009ee368fe
xj63 <xj63work@163.com>
2024-08-03 22:19:10 +0800

    test: Try to add an analytical solution to the box-pushing problem

--- simulate/push-box.c
+++ simulate/push-box.c
@@ -21,8 +21,10 @@ const float AIR_DENSITY = 1.225; // kg/m^3
 const float BOX_AREA = 0.1;      // m^2
 
 /// F_air_resistance = sign(v) * 1/2 * C * rho * S * v^2
+const float AIR_RESISTANCE_SCALE =
+    0.5 * AIR_DRAG_COEFFICIENT * AIR_DENSITY * BOX_AREA; // 1/2 * C * rho * S
 #define AIR_RESISTANCE_FORMULA(v)                                              \
-  (-SIGN(v) * (0.5 * AIR_DRAG_COEFFICIENT * AIR_DENSITY * BOX_AREA * SQUARE(v)))
+  (-SIGN(v) * (AIR_RESISTANCE_SCALE * SQUARE(v)))
 
 /// Simulate push the box in the direction of the force.
 /// Right is the positive direction.
@@ -35,14 +37,24 @@ float push_box(float *speed_addr, float force, float dt) {
     return 0.0;
   }
 
-  float kf_force;
-  if (ABS(speed) < 1e-6)
-    // When the object moves at a very low speed, the direction of movement is
-    // almost determined by the thrust.
-    kf_force = -SIGN(force) * KINETIC_FRICTION_FORCE;
-  else
-    // Sliding friction is opposite to the direction of motion.
-    kf_force = -SIGN(speed) * KINETIC_FRICTION_FORCE;
+  // Sliding friction is opposite to the direction of motion.
+  float kf_force = -SIGN(speed) * KINETIC_FRICTION_FORCE;
+
+  // m * dv/dt == F - f - kv^2
+  // ∫ m/(F-f-kv^2) dv == ∫ dt
+
+  // const_force = F - f
+  float const_force = force - kf_force;
+
+  if (const_force >= 0) {
+    float A = const_force;
+    // ∫ m/(A-kv^2) dv == m/A * ∫ 1/(1-kv^2/A) dv
+    // v^2 == (A/k) * sin(theta)
+    // m/A * arcsin(v/sqrt(A/k)) / sqrt(A/k) == t+C
+  } else {
+    float B = const_force;
+    // ∫ -m/B * arsinh(v/sqrt(B/k))/sqrt(B/k) == t+C
+  }
 
   // Air resistance is proportional to the square of the speed
   float air_resistance = AIR_RESISTANCE_FORMULA(speed);
