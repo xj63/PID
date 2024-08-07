ffafbbce6c95feb10b728b98ddd7e24ec2cdb673
xj63 <xj63work@163.com>
2024-08-07 21:01:29 +0800

    feat: example push box use interrupt callback

--- examples/push-box.c
+++ examples/push-box.c
@@ -1,21 +1,29 @@
 #include "../simulate/push-box.h"
 #include "../pid.h"
-
 #include <stdio.h>
-int main() {
-  struct Pid pid = pid_new_with_integral_clamp(1, 2, 0.1, -300, 300);
 
-  float time = 0.0;
-  float speed = 0.0;
-  float force = 0.0;
-  const float dt = 1;
-  const float target = 100.0;
+static struct Pid pid;
+static const float target = 100.0;
+static const float dt = 1;
+static float speed = 0.0;
+static float force = 0.0;
+static float time = 0.0;
+
+void interrupt_callback() {
+  force = pid_update(&pid, target, speed, dt);
+
+  push_box(&speed, force, dt);
+  printf("time: %.0fs, \t speed: %.1f, \t force: %.2f\n", time, speed, force);
+
+  time += dt;
+}
+
+int main() {
+  pid = pid_new_with_integral_clamp(1, 2, 0.1, -300, 300);
 
   for (unsigned i = 0; i < 10; i++) {
-    push_box(&speed, force, dt);
-    printf("time: %.0fs, \t speed: %.1f, \t force: %.2f\n", time, speed, force);
-
-    force = pid_update(&pid, target, speed, dt);
-    time += dt;
+    // need #include <unistd.h>
+    // sleep(1);
+    interrupt_callback();
   }
 }
